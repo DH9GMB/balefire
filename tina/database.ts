971c2334653621a53fddb5531afda6ad6fb9b695
nikolai-in <nikolai.in@tuta.io>
2024-09-22 10:38:31 +0300

    wip 🚧(Tina): Update config to use MongoDB
    Signed-off-by: nikolai-in <nikolai.in@tuta.io>

--- tina/database.ts
+++ tina/database.ts
@@ -1,50 +1,39 @@
 import { createDatabase, createLocalDatabase } from "@tinacms/datalayer";
-
-// Change this to your chosen git provider
+import { MongodbLevel } from "mongodb-level";
 import { GitHubProvider } from "tinacms-gitprovider-github";
+import dotenv from "dotenv";
 
-// Change this to your chosen database adapter
-import { Redis } from "@upstash/redis";
-import { RedisLevel } from "upstash-redis-level";
+dotenv.config();
 
-// Manage this flag in your CI/CD pipeline and make sure it is set to false in production
+if (!process.env.TINA_PUBLIC_IS_LOCAL) {
+  console.warn("TINA_PUBLIC_IS_LOCAL is not defined")
+}
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI must be defined")
+}
+
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
 const branch =
-  process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || "master";
-
-if (!branch) {
-  throw new Error(
-    "No branch found. Make sure that you have set the GITHUB_BRANCH or process.env.VERCEL_GIT_COMMIT_REF environment variable."
-  );
-}
+  process.env.GITHUB_BRANCH ||
+  process.env.GITHUB_REF ||
+  process.env.HEAD ||
+  "main";
 
 export default isLocal
-  ? // If we are running locally, use a local database that stores data in memory and writes to the locac filesystem on save
-    createLocalDatabase()
-  : // If we are not running locally, use a database that stores data in redis and Saves data to github
-    createDatabase({
-      // May very depending on your git provider
+  ? createLocalDatabase()
+  : createDatabase({
       gitProvider: new GitHubProvider({
-        repo:
-          process.env.GITHUB_REPO ||
-          process.env.VERCEL_GIT_REPO_SLUG ||
-          "default_repo",
-        owner:
-          process.env.GITHUB_OWNER ||
-          process.env.VERCEL_GIT_REPO_OWNER ||
-          "default_owner",
-        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN || "default_token",
         branch,
+        owner: process.env.GITHUB_OWNER!,
+        repo: process.env.GITHUB_REPO!,
+        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
       }),
-      // May very depending on your database adapter
-      databaseAdapter: new RedisLevel<string, Record<string, any>>({
-        redis: new Redis({
-          url:
-            (process.env.KV_REST_API_URL as string) || "http://localhost:8079",
-          token: (process.env.KV_REST_API_TOKEN as string) || "example_token",
-        }) as any,
-        debug: process.env.DEBUG === "true" || false,
-        namespace: branch,
+      databaseAdapter: new MongodbLevel<string, Record<string, unknown>>({
+        collectionName: 'tinacms',
+        dbName: "tinacms-self-host",
+        mongoUri: process.env.MONGODB_URI!,
       }),
+      namespace: branch,
     });
